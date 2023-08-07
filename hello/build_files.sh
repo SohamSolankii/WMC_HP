3be247b7f83d18945c8f60b04d31b13dd591c58a
Soham Solanki <sohammsolanki@gmail.com>
2023-08-07 18:45:58 +0530

    update: few changes

--- hello/build_files.sh
+++ hello/build_files.sh
@@ -1,5 +1,8 @@
-echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
+#!/usr/bin/env bash
 
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
